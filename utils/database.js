9ae7bc0976a6c01a6dc1407f6fea762c2816f342
rahulxo19 <rahulchhikara2001@gmail.com>
2023-06-14 16:08:34 +0530

    adding Readme.

--- utils/database.js
+++ utils/database.js
@@ -1,8 +1,13 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("restaurantapp", "root", "Qawsed@123", {
-  dialect: "mysql",
-  host: "localhost",
-});
+const sequelize = new Sequelize(
+  "restaurantapp",
+  "<user_name>",
+  "<user_password>",
+  {
+    dialect: "mysql",
+    host: "localhost",
+  }
+);
 
 module.exports = sequelize;
